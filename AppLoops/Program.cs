ebfe447dd83bf24b01aed1062c13d77125c2c0f0
{dp7jm} <{dpjm@chef.net}>
2019-08-27 04:47:04 +0200

    Add project files.

---
+++ AppLoops/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chosenNumber = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(chosenNumber);
+
+        }
+    }
+}
